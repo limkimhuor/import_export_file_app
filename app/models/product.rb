41f6c4f6aee2b219374656a4b92295ef8aab0931
limkimhuor <kimhuorlim@gmail.com>
2017-05-19 13:50:08 +0700

    import product with validation price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  validates_presence_of :price
+
   class << self
     def to_csv(options = {})
       CSV.generate(options) do |csv|
@@ -6,34 +8,6 @@ class Product < ApplicationRecord
         all.each do |product|
           csv << product.attributes.values_at(*column_names)
         end
-      end
-    end
-
-    def import file
-      #import spreadsheet
-      spreadsheet = open_spreadsheet(file)
-      header = spreadsheet.row(1)
-      (2..spreadsheet.last_row).each do |i|
-        row = Hash[[header, spreadsheet.row(i)].transpose]
-        product = find_by_id(row["id"]) || new
-        product.attributes = row.to_hash
-        product.save!
-      end
-
-      # CSV.foreach(file.path, headers: true) do |row|
-      #   # Product.create! row.to_hash # import new
-      #   product = find_by_id(row["id"]) || new
-      #   product.attributes = row.to_hash
-      #   product.save!
-      # end
-    end
-
-    def open_spreadsheet file
-      case File.extname(file.original_filename)
-      when ".csv" then Roo::Csv.new(file.path, nil, :ignore)
-      when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
-      when ".xlsx" then Roo::Excelx.new(file.path, file_warning: :ignore)
-      else raise "Unknow file type: #{file.original_filename}"
       end
     end
   end
---
+++ app/models/product_import.rb
@@ -0,0 +1,55 @@
+require "roo"
+
+class ProductImport
+  extend ActiveModel::Model
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
+
+  attr_accessor :file
+
+  def initialize(attributes = {})
+    attributes.each { |name, value| send("#{name}=", value) }
+  end
+
+  def persisted?
+    false
+  end
+
+  def save
+    if imported_products.map(&:valid?).all?
+      imported_products.each(&:save!)
+      true
+    else
+      imported_products.each_with_index do |product, index|
+        product.errors.full_messages.each do |message|
+          errors.add :base, "Row #{index+2}: #{message}"
+        end
+      end
+      false
+    end
+  end
+
+  def imported_products
+    @imported_products ||= load_imported_products
+  end
+
+  def load_imported_products
+    spreadsheet = open_spreadsheet
+    header = spreadsheet.row(1)
+    (2..spreadsheet.last_row).map do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      product = Product.find_by(id: row["id"]) || Product.new
+      product.attributes = row.to_hash
+      product
+    end
+  end
+
+  def open_spreadsheet
+    case File.extname(file.original_filename)
+    when ".csv" then Roo::CSV.new file.path
+    when ".xls" then Roo::Excel.new file.path
+    when ".xlsx" then Roo::Excelx.new file.path
+    else raise "Unknow file type: #{file.original_filename}"
+    end
+  end
+end
